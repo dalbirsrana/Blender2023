dd0287caf4a88634885827a51b88de451c50519a
Dalbir Singh <dalbirsingh@Dalbirs-MacBook-Pro.local>
2023-05-16 11:19:09 -0700

    community page title section update

--- single-community.php
+++ single-community.php
@@ -11,7 +11,6 @@ get_header();
 //ACF Fields
 $contact_info = get_field('contact_info');
 $property_pictures = get_field('gallery');
-
 ?>
 	<main id="primary" class="site-main page-community">
 
@@ -19,9 +18,14 @@ $property_pictures = get_field('gallery');
             <div class="breadcrumb"><a href="/communities/"><i class="fa fa-chevron-left"></i> <span>View all communities</span></a></div>
         </div>
 
+        <?php
+            $total_pictures = has_post_thumbnail() ? count(array_filter($property_pictures)) + 1 : count(array_filter($property_pictures));
+        ?>
+
         <div class="slider-wrap">
-            <div id="property_pictures_counter"><span class="cur-slide">1</span>/<?php echo count(array_filter($property_pictures)); ?></div>
+            <div id="property_pictures_counter" class="slide-counter"><span class="cur-slide">1</span> / <?php echo $total_pictures; ?></div>
             <div id="slider_property_pictures">
+                <?php the_post_thumbnail( 'medium', array( 'class' => 'featured-image' ) ); ?>
                 <?php
                     foreach ($property_pictures as $field => $image) {
                         echo wp_get_attachment_image( $image, 'medium', "", array( "class" => $field ) );
@@ -35,16 +39,33 @@ $property_pictures = get_field('gallery');
                 <div class="title-content">
                     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                     <div class="contact">
-                        <span class="address"><?php echo $contact_info['street'].', '.$contact_info['city'].', '.$contact_info['province']['label']; ?></span>
-                        <span class="phone"><?php echo $contact_info['phone']; ?></span>
+                        <span class="address"><i class="fas fa-external-link-square-alt"></i>
+                            <a href="#">
+                                <?php echo $contact_info['street'].', '.$contact_info['city'].', '.$contact_info['province']['label']; ?>
+                            </a>
+                        </span>
+                        <span class="phone"><i class="fas fa-phone-alt"></i>
+                            <a href="tel:<?php echo $contact_info['phone']; ?>">
+                                <?php echo $contact_info['phone']; ?>
+                            </a>
+                        </span>
                     </div>
                     <div class="inquire">
-                        <a href="mailto:<?php echo $contact_info['contact_person_email']; ?>" class="button inquire">Inquire</a>
+                        <a href="mailto:<?php echo $contact_info['contact_person_email']; ?>" class="button mailto"><i class="fas fa-envelope"></i> Inquire</a>
                     </div>
                 </div>
 
                 
-                <div id="gallery_wrap" class="property-pictures <?php if ( count(array_filter($property_pictures)) > 4 ) {echo 'gallery';} else {echo 'single';} ?>">
+                <div id="gallery_wrap" class="property-pictures <?php if ( $total_pictures > 4 ) {echo 'gallery';} else {echo 'single';} ?>">
+
+                <?php 
+                    if ( has_post_thumbnail() ) {
+                        echo '<a data-fancybox="gallery" href="'. esc_url( get_the_post_thumbnail_url() ) .'">';
+                        the_post_thumbnail( 'medium', array( 'class' => 'featured-image' ) );
+                        echo '</a>';
+                    }
+                 ?>
+
                 <?php
                     foreach ($property_pictures as $field => $image) {
                         if ( !empty($image) ) {
@@ -62,17 +83,51 @@ $property_pictures = get_field('gallery');
 		</header><!-- .page-header -->
 
 
-		<section class="single-post">
+		<section class="property-intro">
             <div class="container">
-				<?php
-				while ( have_posts() ) : the_post();
-
-					get_template_part( 'template-parts/content', 'single' );
-
+			    <?php
+                    while ( have_posts() ) : 
+                    the_post();
+                ?>
+                    <div class="entry-content">
+                        <?php
+                            the_content(
+                                sprintf(
+                                    wp_kses(
+                                        /* translators: %s: Name of current post. Only visible to screen readers */
+                                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'blender2023' ),
+                                        array(
+                                            'span' => array(
+                                                'class' => array(),
+                                            ),
+                                        )
+                                    ),
+                                    wp_kses_post( get_the_title() )
+                                )
+                            );
+                        ?>
+                    </div><!-- .entry-content -->
+                <?php
 				endwhile; // End of the loop.
 				?>
 			</div>
 		</section>
+
+
+
+
+        <?php $what_to_expect = get_field('what_to_expect'); ?>
+        <?php if ( $what_to_expect ) : ?>
+            <section class="what-to-expect">
+                <div class="container">
+                    <?php echo $what_to_expect; ?>
+                </div>
+            </section>
+        <?php endif; ?>
+
+
+
+
 
 	</main><!-- #main -->
 
